b5546a5e35b5caef8c85bbeadb9771c12029c834
raul-rauqoz <raul.quinonez@telus.com>
2022-04-12 14:37:47 -0600

    feat: swagger

--- backend/Routes/routes.js
+++ backend/Routes/routes.js
@@ -2,12 +2,62 @@ const routes = require('express').Router();
 const all_messages = require('../messages/messages');
 
 routes.get('/', (req, res) => {
-	res.status(200).send({ data: true });
+	res.status(200).send(true);
 });
 
+/**
+* @swagger
+*components:
+*  schemas:
+*    Message:
+*      type: object
+*      properties:
+*        nick:
+*          type: string
+*          description: nick del usuario
+*        message:
+*          type: string
+*          description: mensaje del usuario
+*      example:
+*        nick: Raul
+*        message: Hola
+*/
+
+/** 
+* @swagger
+*paths:
+*  /messages:
+*    get:
+*      tags: [Message - Get]
+*      description: Obtener los mensajes
+*      responses:
+*        '200':
+*          description: Mensajes
+*          content:
+*            application/json:
+*              schema:
+*               type: array
+*               items:
+*                 $ref: '#/components/schemas/Message'
+*/
 routes.get('/messages', (req, res) => {
 	res.status(200).send(all_messages);
 });
+
+/** 
+* @swagger
+*paths:
+*  /delete_all:
+*    delete:
+*      tags: [Message - Delete]
+*      description: Borrar los mensajes
+*      responses:
+*        '200':
+*          description: Mensajes Limpios
+*          content:
+*            application/json:
+*              example: []
+*/
 routes.delete('/delete_all', (req, res) => {
 	all_messages.splice(0, all_messages.length);
 	res.status(200).send(all_messages);
--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,30 @@ const cors = require('cors');
 const routes = require('./Routes/routes');
 const app = express();
 const port = process.env.PORT || 4000;
+const swaggerIU = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
+const options = {
+	definition: {
+		openapi: '3.0.1',
+		info: {
+			title: 'Chat Rauqoz',
+			description: 'Documentation',
+			version: '1.0.0'
+		},
+		servers: [
+			{
+				url: 'http://localhost:4000'
+			}
+		]
+	},
+	apis: [ './Routes/routes.js' ] // files containing annotations as above
+};
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 app.use('/', routes);
+app.use('/api-doc', swaggerIU.serve, swaggerIU.setup(swaggerJsdoc(options)));
 
 const server = app.listen(port, (err) => {
 	if (err) {
